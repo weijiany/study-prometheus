3fca7352c03b52b38c36ef507377a8ba567dba78
weijian <weijian.yang@thoughtworks.com>
2022-01-05 11:09:09 +0800

    add spring boot service to exposure prometheus metrics.

---
+++ test-actuator/src/main/java/com/prometheus/testactuator/TestActuatorApplication.java
@@ -0,0 +1,13 @@
+package com.prometheus.testactuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestActuatorApplication.class, args);
+	}
+
+}
---
+++ test-actuator/src/test/java/com/prometheus/testactuator/TestActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prometheus.testactuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestActuatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
